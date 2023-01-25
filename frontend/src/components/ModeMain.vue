6eb147b93fece4e0fe812fe7ad9eb284106b934f
임태민 <devtaemin@gmail.com>
2023-01-25 17:44:47 +0900

    [FE] 솔로모드 라우팅 및 다이얼로그 추가
    Former-commit-id: d1df390bf60e75dd0220dba5a91ec31f324a6fac

--- frontend/src/components/ModeMain.vue
+++ frontend/src/components/ModeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dc99a69bf450b569afdce15560205e77af2b9a8cbae90e955f91b328aeebe3a
-size 181
+oid sha256:53b1efd8440a2fe443383154c9be8b631acc94a9b5e5d1b8173b6ebbd8c33e78
+size 3047
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e53090609ff2d7118dc13b7f9ca406dd4e656bc87e3e1e95fd9c075e167e3d3
-size 814
+oid sha256:0fe5fa44bf64908ead8f4708a493976bd8dbd1f96655c10ae65188cac9f787fc
+size 1111
