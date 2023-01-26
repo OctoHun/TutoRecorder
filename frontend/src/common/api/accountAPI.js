9991bc5d300f71b7d86b97912d163378028d52ad
임태민 <devtaemin@gmail.com>
2023-01-26 13:34:28 +0900

    [MR] : 스켈레톤 코드 수정
    Former-commit-id: 097f9c91bd0cbc64dfdb7a852feaf6471f31dc9c

--- frontend/src/common/api/accountAPI.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6ffd1d377c97b55f02ef0334b028e8ef3660b12aa2760afb0ed5ce48dd5839eb
-size 565
---
+++ frontend/src/views/OpenViduTestView.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c298271ce18d47a63d86613400336077039ef6d06bfeb757b911ccd0e851bf1
+size 5077
