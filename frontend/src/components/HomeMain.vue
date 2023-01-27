50a60cba0b27b8ca3c4ceb13ffe8fd0f0eb1e744
OctoHun <tjd_gns12@naver.com>
2023-01-27 16:04:26 +0900

    FE-FEAT : openvidu 관련 컴포넌트 수정(#13)
    Former-commit-id: 7e295fe14051db3c571080debcfd88f068972472

--- frontend/src/components/HomeMain.vue
+++ frontend/src/components/HomeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af804409739abf9fd0a77a9de828eae9e65da710f643f791068fa3036eee23ad
-size 1303
+oid sha256:39a04f72e1bb053e96e36ce8c6b0da02721eb699361fdb550f8c391053676954
+size 1351
