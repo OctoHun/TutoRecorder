fdbd90b9173e19a686558c458139ae51ffb67937
OctoHun <tjd_gns12@naver.com>
2023-01-29 20:45:24 +0900

    FE-FEAT : SoloSound 수정(#15)
    Former-commit-id: 291c53b6ccb10003d11ae48ce5e99cbab02dc5ab

--- frontend/src/components/solo/SoloSoundMain.vue
+++ frontend/src/components/solo/SoloSoundMain.vue
@@ -1,13 +1,3 @@
-<template>
-    <div>
-        <h1>{{ msg }}</h1>
-    </div>
-</template>
-<script>
-export default {
-    name: 'SoloSoundMain',
-    props: {
-        msg: String
-    }
-}
-</script>
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c78049daed22360f79eb8b9b0e0acd9a78c506130047f58316a5e45f923d2d1
+size 1018
