795baed12ed6e429924a7dc853801f7f8b498e5f
njh3321 <njh3321@naver.com>
2023-01-30 17:50:42 +0900

    FE-FIX : 배경 캐릭터가 위에 떠오르는 문제 해결 중(#21)
    Former-commit-id: 2dac2fce89aca2a2b12cb544584b6f7c37944784

--- frontend/src/components/HomeMain.vue
+++ frontend/src/components/HomeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a57452d5a0e4ca4736310a0538bcbf25cf830e5d229437bbd805f239794c1dec
-size 1327
+oid sha256:e7cf6b0b35b8b3e81e0bcf83ec86af90e03370eab8f7f01d5bc89a4e09cb550e
+size 1295
--- frontend/src/components/ModeMain.vue
+++ frontend/src/components/ModeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25bb5d98dce4b1c098aef82a0a9c6c189f8779864a6b245cc67033d195bfaf1c
-size 3118
+oid sha256:3ce4ec211dd59a42eea3360faf45a80e8be4596fc815fcc307abe56fb3b96fd4
+size 3077
