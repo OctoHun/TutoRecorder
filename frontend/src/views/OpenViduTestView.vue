b412b9f246561540ac36076fc25807323a92179c
pjt laptop <munhwa3278@gmail.com>
2023-01-30 17:36:03 +0900

    FE-FEAT : game setting modal function
    Former-commit-id: ffa90384992ff58f1aee5ddab836715b253f8afe

--- frontend/src/views/OpenViduTestView.vue
+++ frontend/src/views/OpenViduTestView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f93d7cf85ed02d7d40f7f7e253eb87b50c53adfca6154f16f9886d33d481ad9
-size 9465
+oid sha256:9c6fbb2cd4f76e8109d47d7d114bda01ed835e9530d1e767929971039ed267c3
+size 11402
