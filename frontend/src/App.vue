18d46117ee29586ea395f935b268b70261e2ca51
njh3321 <njh3321@naver.com>
2023-01-30 10:37:32 +0900

    FE-FIX : App에 메인페이지 배경화면 수정을 위한 준비(#17)
    Former-commit-id: 08abd62f941616d11e7522c12bc2e05037d26f58

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5e4a6c6abbfd68151f44d2879d3860eb2fde45fe2bd6bd2223c331bac5c7881
-size 430
+oid sha256:87e6a7ac347e47f5618ccccab82b43577d1956450cd19095a3c7ba84e844ad3d
+size 593
