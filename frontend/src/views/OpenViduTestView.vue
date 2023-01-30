76f0215f713d99317a9d9cb507e9a485341f8559
pjt laptop <munhwa3278@gmail.com>
2023-01-30 15:41:27 +0900

    FE-FIX : volume sldier update, restructuring
    Former-commit-id: 4e419d2f23b3549e9db716799a18a4cacdbd50e3

--- frontend/src/views/OpenViduTestView.vue
+++ frontend/src/views/OpenViduTestView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e88a46583640c9bbdb858af3ec8b1b772cf3e0d5366c3412910ada4595ff3149
-size 9438
+oid sha256:9f93d7cf85ed02d7d40f7f7e253eb87b50c53adfca6154f16f9886d33d481ad9
+size 9465
