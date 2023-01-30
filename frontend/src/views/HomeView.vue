53d661b169a664cb50a2cff9679913bba62a9d27
pjt laptop <munhwa3278@gmail.com>
2023-01-30 11:20:30 +0900

    FE-FEAT : 환경설정 뼈대 구축 (#18)
    Former-commit-id: 7a6fcbe4fda0e50c0476a6611de63cf96d0872a4

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b821f2bc8a8bdca4347e5c2f173d416322d4b8587cb65775cdedaf9f6dbfaa01
-size 226
+<template>
+  <div class="home">
+    <HomeMain msg="홈페이지"/>
+  </div>
+</template>
+
+<script>
+import HomeMain from '@/components/HomeMain'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HomeMain
+  }
+}
+</script>
