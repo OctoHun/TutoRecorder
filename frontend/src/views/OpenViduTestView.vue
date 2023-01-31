c7fa1acf7bc7d9080197714800616167c0fbbf0a
pjt laptop <munhwa3278@gmail.com>
2023-01-31 10:45:31 +0900

    FE-FEAT: 같이하기 권한에 따른 보이는 화면 다르게 하기
    Former-commit-id: 2f79996271e611e887582120a6cb63454c603ac7

--- frontend/src/views/OpenViduTestView.vue
+++ frontend/src/views/OpenViduTestView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c6fbb2cd4f76e8109d47d7d114bda01ed835e9530d1e767929971039ed267c3
-size 11402
+oid sha256:bff7b02d2b5d1e0e2b94ff84a46a0544d44728e6ebc5aa02ea5fdfb638d6d92e
+size 11969
