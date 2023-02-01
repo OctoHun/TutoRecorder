014babd621fd8252208d5b15b8eb52d3f77af497
pjt laptop <munhwa3278@gmail.com>
2023-02-01 23:57:34 +0900

    FE-FEAT : 스크롤 추적 기능 구현/ n 명일 때 추적 기능 보완 필요
    Former-commit-id: 0446343a033ff02cd2002f63055581a56e37872d

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e46c31d5ba1f7d875d08d1ef966904dfaae53a3b41429f5814b04a6df8fec62e
-size 23701
+oid sha256:a3ffb8f5ac4779ba06884ee39a21a98942f6c5a54da40ebc100880b24b0406b5
+size 25724
