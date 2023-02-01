3b2b8348661e421d689f93e991432948db0f6dd7
pjt laptop <munhwa3278@gmail.com>
2023-02-02 01:16:18 +0900

    FE-FEAT : 해상도 수정, 유저 목록 높이 수정, 채팅창+입력창+시작버튼 크기 수정
    Former-commit-id: 7d43eb43e57109f96d276b9b7d01f5cd42ee8891

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3ffb8f5ac4779ba06884ee39a21a98942f6c5a54da40ebc100880b24b0406b5
-size 25724
+oid sha256:7b20457dddbd203a7916bdc60aa2c2795b2d197e4021ac33135719c355a4eaab
+size 26649
