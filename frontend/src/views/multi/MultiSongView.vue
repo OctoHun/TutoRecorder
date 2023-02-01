fbdf44eb47f198a4e1a95ac69ff40a2fea00150e
OctoHun <tjd_gns12@naver.com>
2023-02-01 09:27:14 +0900

    FE-FEAT : multisong 디자인 수정(#25)
    Former-commit-id: 3b595c9bd5d3aa418fd134445ecaee9842457858

--- frontend/src/views/multi/MultiSongView.vue
+++ frontend/src/views/multi/MultiSongView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2481fcb1d7ff1145ca2c33b15fb3cf865b5a11ba3181e6c4a8e462ec0b57a281
-size 289
+oid sha256:d88440b3630e8adcbdfafd6005920aae3a2f6a722bd562b0e40df45b35c46cdd
+size 608
