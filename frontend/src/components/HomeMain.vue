966d6176b15d0393e2b35863dddc3cecaa9600b3
pjt laptop <munhwa3278@gmail.com>
2023-02-01 09:24:40 +0900

    FE-FEAT : 홈 화면 정립
    Former-commit-id: 290cee916878dcf9e5f83f31c76c80b8e1e9c130

--- frontend/src/components/HomeMain.vue
+++ frontend/src/components/HomeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:237f55cb4ab7dd3e576aab8498ccf611478f990482cf407c9b8664250b5aebcb
-size 1604
+oid sha256:4f4323451f89f541b0f72e8d4a1726b0f73659e39ffcbb79eac95804e4b2005c
+size 1609
