e3289a49c38c8461941d993d84105a814dc90daf
임태민 <devtaemin@gmail.com>
2023-02-02 11:20:30 +0900

    FE-FIX : merge를 위한 코드 수정
    Former-commit-id: d9974a6fc16148654710577d625f5d18db797265

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80e31c4f1e60af3a751252496338681000513aed231aa6fc3adca59aec876932
-size 25704
+oid sha256:2ba9e8839624eba6796cb3e7d48aa2e3eef5dc11dad694b81865b61fbc576728
+size 31090
