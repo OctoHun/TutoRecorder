0b7143433ff58d61b4acc57ef2bdbeb385eab209
OctoHun <tjd_gns12@naver.com>
2023-02-02 17:55:36 +0900

    FE-FEAT : 게임 넣고 분석컴포넌트에 점수 적용, 아직 게임 안됨(#31)
    Former-commit-id: 20d2df3be57a4f14536be87eed8f7b4a7115c8e7

--- frontend/src/components/solo/SoloSoundMain.vue
+++ frontend/src/components/solo/SoloSoundMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fc987de00de2e38d49042c32f56dee44799e91afb06bf5b6eb48e16a155bd34
-size 4198
+oid sha256:9ff97e25737f94a5af12a8baa437490aebde710efb21bb3d138b7e217a6acbaf
+size 8085
