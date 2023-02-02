1a30431008add204cef05a5e003e4cf98b61d9ed
njh3321 <njh3321@naver.com>
2023-02-02 17:56:05 +0900

    FE-FEAT : 시작버튼 애니메이션 효과 추가(#34)
    Former-commit-id: b787b20d336d7e0e6041dbe2846fbf6919500694

--- frontend/src/components/HomeMain.vue
+++ frontend/src/components/HomeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3055e276f03d4fc323064d67906df52eb29a59ac4247e8a2e73948e325bc8f2b
-size 2711
+oid sha256:36fba331f5c97294f5ba6666e436b7f11f6c328bde6f5b9f1e1f726d1997dbab
+size 3588
