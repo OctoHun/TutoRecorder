b24bf0566098ca0c03d7460fa6727ae31c4645b4
OctoHun <tjd_gns12@naver.com>
2023-02-02 09:41:19 +0900

    FE-FEAT : 기능만 추가(#31)
    Former-commit-id: e14055aef683f74007694ae873ffdfc3b8e87b12

--- frontend/src/components/solo/SoloSoundMain.vue
+++ frontend/src/components/solo/SoloSoundMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a18c28850bba19a5e7b029bbd51cef07794724fd7c84c52f168d38b5b5ac2e20
-size 1626
+oid sha256:8fc987de00de2e38d49042c32f56dee44799e91afb06bf5b6eb48e16a155bd34
+size 4198
