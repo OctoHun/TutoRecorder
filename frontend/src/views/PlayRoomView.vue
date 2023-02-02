030812422412bb4a13483ba9dfee5f175ceba7b3
임태민 <devtaemin@gmail.com>
2023-02-02 10:10:04 +0900

    FE-FEAT feat-front/playroom 비디오 및 오디오 온오프 기능 구현
    Former-commit-id: 197599b2aa7de39bc3bf20ab8938698ab9e7994e

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a875e006df417830532de2a6b68e7b1c70e3bc18790732665ca31e678cce0d6
-size 28676
+oid sha256:b9853dfebb5f6248473b8addaf05aa5b567ac58c729355f98bb9b7f123be4d58
+size 30573
