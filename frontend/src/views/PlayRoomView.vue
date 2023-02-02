15d3fc662c5f0acc11bb188ce0461daa7d359ef8
pjt laptop <munhwa3278@gmail.com>
2023-02-02 10:01:06 +0900

    FE-FEAT : n명일 때 스크롤바 추적 기능 추가
    Former-commit-id: 704749d14da2bebd3f57868ba14320bc075ddf74

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b20457dddbd203a7916bdc60aa2c2795b2d197e4021ac33135719c355a4eaab
-size 26649
+oid sha256:80e31c4f1e60af3a751252496338681000513aed231aa6fc3adca59aec876932
+size 25704
