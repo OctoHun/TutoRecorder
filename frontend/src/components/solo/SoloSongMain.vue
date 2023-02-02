72b97d64d34e5b7234303dcaf699f755996fb4cd
njh3321 <njh3321@naver.com>
2023-02-02 15:45:54 +0900

    FE-FEAT : 혼자하기-연주하기에 내 카메라 추가 및 디자인(#32)
    Former-commit-id: fbe02f604fcf4ef7c92089dbc448e8e4777674fe

--- frontend/src/components/solo/SoloSongMain.vue
+++ frontend/src/components/solo/SoloSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ac8a1489ec5cc249f1bfc045595bc69f6999517be8a6127947670c046f7bde5
-size 3082
+oid sha256:e8f62bffce24d58a80caae3304237e5aabc58c358c96f050c0425f168ac8a0e1
+size 8186
---
+++ frontend/src/components/video/solo/soloSongUserVideo.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:deea0adcd73a32182c012808b9851ab8dc724da17c61af8df32db21397976cca
+size 936
