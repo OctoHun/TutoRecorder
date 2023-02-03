add9e3c1715c463b0f0528d2df5ce6659b2acff3
pjt laptop <munhwa3278@gmail.com>
2023-02-03 17:42:37 +0900

    FIX-FRONT : 혼자하기 위치 css 수정 중/  카드 내부 비율 및 여백 유지, 최소화에서 로고 사이즈 조정 필요
    Former-commit-id: 6bbccf77fcf54202d876f54dc4d12fd909b1f619

--- frontend/src/components/solo/SoloMain.vue
+++ frontend/src/components/solo/SoloMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db137daac66641e255799115114198dace6e4bc2a67f874749289f2a3527b61a
-size 3143
+oid sha256:53ded20602c89e5cb41970abb3818ae1ede66e541270aa27def0dc728359d26e
+size 3547
