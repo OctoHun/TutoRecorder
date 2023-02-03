6d5e5bd6f627cb10b40cd7e1bc0570965d2154c6
OctoHun <tjd_gns12@naver.com>
2023-02-03 10:50:14 +0900

    FE-FEAT : 혼자하기-소리내기 게임 기능 테스트(#31)
    Former-commit-id: d24c6f1fdd0b733f2d261c19a58f795bbff56aa0

--- frontend/src/components/solo/SoloSoundMain.vue
+++ frontend/src/components/solo/SoloSoundMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ff97e25737f94a5af12a8baa437490aebde710efb21bb3d138b7e217a6acbaf
-size 8085
+oid sha256:0d39b63e131a155145bb09e60b283268e451e751d03aa28927cbb674f54ef0f7
+size 8124
