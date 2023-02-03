5773a6d3b90322faf16f347b0c47ad595fd89602
pjt laptop <munhwa3278@gmail.com>
2023-02-03 10:19:03 +0900

    FIX-FRONT : 참여코드 입력 창 디자인 수정 및 에러 기능 추가 완료, 유효성 검사 부분 수정 필요
    Former-commit-id: 34244ddd165a1a51a97693d6e3b76c380820332a

--- frontend/src/components/HomeMain.vue
+++ frontend/src/components/HomeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3055e276f03d4fc323064d67906df52eb29a59ac4247e8a2e73948e325bc8f2b
-size 2711
+oid sha256:92fa7136c52f67cf5eba93e37b53bf2d9d2e1884d00fd32b197601c03fdce8de
+size 2648
