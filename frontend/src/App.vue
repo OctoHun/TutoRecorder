9d3e43c52c19484493398149910422864410c1ce
njh3321 <njh3321@naver.com>
2023-02-03 09:15:46 +0900

    FE-FIX : App.vue 애니메이션에 여러 웹 브라우져 호환성 추가(#34)
    Former-commit-id: 3d8af5d495a6b99a6c924484a2852c936bc79db0

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1470f89ba8a7f01f4b09378f1f4e60ab175e04108c373cfc7c89ad578eca451d
-size 5820
+oid sha256:7f6251c0493c9c473aa70348bdac95c907dd571dbe6e58486e72dd8cf6f75ecc
+size 8965
