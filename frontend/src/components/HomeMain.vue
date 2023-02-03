1013cc05ab58a6841f08db20d0cb4a2e76538c56
njh3321 <njh3321@naver.com>
2023-02-03 17:32:11 +0900

    FE-FEAT : 배경화면 캐릭터 클릭시 애니메이션 추가 및 CSS파일 분리(#34)
    Former-commit-id: 4b25cbf7d7ce85b9f1b481d2133f0687bb225007

--- frontend/src/components/HomeMain.vue
+++ frontend/src/components/HomeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:757009baaed472b7cf21209ec7e33bb18f0432a4ba78390e3f71c6043e7fd92a
-size 3899
+oid sha256:2842567f30b08334f16aa53f270ac5ecc425ea1a58b5e49637960e9f7d92c1f4
+size 2928
