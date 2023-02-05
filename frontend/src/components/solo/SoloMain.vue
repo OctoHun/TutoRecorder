b8b1ec142723bf6c9e8e88d150a3da039d5aaaf2
kim hyeon-seo_h <munhwa95@naver.com>
2023-02-05 18:12:18 +0900

    FIX-FRONT : 이미지 수정,  확인 필요
    Former-commit-id: 03e9e3f48fe75063fd95dc5869cde744a8d12c8f

--- frontend/src/components/solo/SoloMain.vue
+++ frontend/src/components/solo/SoloMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16c18409bb2dd48a8ec4eaa7d4189ae13a92d04a68755f18a250ceb797a24663
-size 4155
+oid sha256:d9e5d85a6db71fbb3809efda8ac6af4a177218c9218c810139d99cac9a539a95
+size 3872
