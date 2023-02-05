db65ed4fefec84019b58a51739c3e85ae7c7e618
노준호 <njh3321@naver.com>
2023-02-05 22:26:45 +0900

    FE-FEAT : 마우스 커서 변경, 같이하기 부분은 구현 안 돼 있음(#34)
    Former-commit-id: fd122befbbdc1ccded338cf3dca93e886a691d18

--- frontend/src/components/HomeMain.vue
+++ frontend/src/components/HomeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f067e7711ce14393d9b77fa7eafd38a587e8e1331f3ce30f785ea2b0b6dccdca
-size 2939
+oid sha256:f5500ffcfbceaa2d92e4d296e3bd7b23dc068c628c358dde9b5eea7a43221d12
+size 3076
