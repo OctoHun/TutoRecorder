d3357961e34aa104d7d89513bb96cb8f698d8c7f
njh3321 <njh3321@naver.com>
2023-02-06 11:05:14 +0900

    FE-FEAT : Carousel 크기 조정(#36)
    Former-commit-id: e39136dd9d66654b049af4b58e274f4c7094b0d1

--- frontend/src/components/multi/MultiSongMain.vue
+++ frontend/src/components/multi/MultiSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaab0dc1c62de8e2cc9d93d5f14f4b5b7dafc10f5387e886955a01ed47eec80b
-size 4343
+oid sha256:286df9782a17f2f9e70407c08cd7dc70df2b1c0abef3db11c027eaf85209e6e6
+size 4340
