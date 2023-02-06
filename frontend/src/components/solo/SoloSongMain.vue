92e0f1ed812dffa2ffcdc1aba4de96aab5c6548f
njh3321 <njh3321@naver.com>
2023-02-06 17:45:55 +0900

    EXTRA : 수정 전 위치 확인(#39)
    Former-commit-id: 58231e5a5286171751cb0a851bedc17827e44a5a

--- frontend/src/components/solo/SoloSongMain.vue
+++ frontend/src/components/solo/SoloSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19ca1236c5d4ed7ed0f292892c3785d9a1dfc5e1ebaea01c59c1633b0fbffd76
-size 31373
+oid sha256:d287fd739a8b315b5ee5569d366b4ef50c92b0878a0780ed00da6d80916c7777
+size 31454
