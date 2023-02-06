27506f955b19ac064348cf90018970254e4c43b4
OctoHun <tjd_gns12@naver.com>
2023-02-06 17:01:40 +0900

    FE-FIX : 오탈자 수정(#31)
    Former-commit-id: fa61daf0de4f74cead7a2c5f231ce62f320424c8

--- frontend/src/components/solo/SoloAnalizeMain.vue
+++ frontend/src/components/solo/SoloAnalizeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7df5f0642a9aef2505b238edb3d512ecc5eb000fccec159c719db2adff040c48
+oid sha256:5589dc9d4fb124336550e7d0331fa8092a3ec17e5ea16345b2a646786dee9903
 size 9715
