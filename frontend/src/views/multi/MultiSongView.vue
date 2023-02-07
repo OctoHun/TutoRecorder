b4b60972dc3b6b983f7dc50f9e8cd27eb47a85b5
OctoHun <tjd_gns12@naver.com>
2023-02-07 13:40:05 +0900

    FE-FIX : 플레이룸 수정(#41)
    Former-commit-id: 51a0b62ddc889bf2927aa8b340356b0b040a41f2

--- frontend/src/views/multi/MultiSongView.vue
+++ frontend/src/views/multi/MultiSongView.vue
@@ -1,21 +1,3 @@
-<template>
-    <div class="pink-container">
-        <MultiSongMain/>
-    </div>
-</template>
-
-<script>
-import MultiSongMain from '@/components/multi/MultiSongMain.vue';
-
-export default {
-    name: 'MultiSongView',
-    components: {
-        MultiSongMain
-    }
-}
-</script>
-
-<style>
-
-
-</style>
+version https://git-lfs.github.com/spec/v1
+oid sha256:b3fa787b70786a8c89d445bd8f76e697ebfb5620927db738a510588ec13b2bbb
+size 290
--- frontend/src/views/multi/MultiSoundView.vue
+++ frontend/src/views/multi/MultiSoundView.vue
@@ -1,20 +1,3 @@
-<template>
-    <div class="pink-container">
-        <MultiSoundMain/>
-    </div>
-</template>
-
-<script>
-import MultiSoundMain from '@/components/multi/MultiSoundMain.vue';
-
-export default {
-    name: 'MultiSoundView',
-    components: {
-        MultiSoundMain,
-    }
-}
-</script>
-
-<style>
-
-</style>
+version https://git-lfs.github.com/spec/v1
+oid sha256:6e1434ab6c976dad7286d02a1407dee84c9f787f892b21ed80c287a8383df641
+size 295
