da251e9ed5a84e4278870ede4d690c6ae442847b
임태민 <devtaemin@gmail.com>
2023-02-08 16:23:47 +0900

    FE-FEAT : 같이하기 동시접속(노래) 카메라 구현(#46)
    Former-commit-id: bc92c7bcba616e20528323dc52f517961c216487

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd30b33fc2e2766ccdb8b7ce10ef0d64e632a7460325b187dabcb561f8302a93
-size 34248
+oid sha256:3af0d5eb273bc6abad50ea8b0662c2c6abb3d85786c28d17cba6d4af4c7700e3
+size 34298
