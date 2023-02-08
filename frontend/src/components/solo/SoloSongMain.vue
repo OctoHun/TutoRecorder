daee0f619cf339d4bcb07b8d5bcdae618c5a0e46
이우승 <ss6116@daum.net>
2023-02-08 09:10:03 +0900

    FE-FIX : 구슬비 곡 추가(#22)
    Former-commit-id: 4949ed558cda998342926991c15e73ecfd33fa23

--- frontend/src/components/solo/SoloSongMain.vue
+++ frontend/src/components/solo/SoloSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d1743a9c5f8096157c54429ceaf1be3dfdf35f1c0e2d0e55ecebe8aacac6afc
-size 31808
+oid sha256:8917eb69f31f6b1b76645fab36fbe8a2cc16db2f2a81e7388a10d9d45185c659
+size 32298
