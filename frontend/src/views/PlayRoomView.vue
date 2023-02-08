25ad71216d14912cc49673861291dfca1ce45399
pjt laptop <munhwa3278@gmail.com>
2023-02-08 13:27:52 +0900

    FE-FIX : 같이하기/채팅 디자인 수정(#45)
    Former-commit-id: e92c78b76d194be7091db71b360de75a0beb016b

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4565b12c6cce38880f37c55216902a8ebbacec9df0e0b104005e7a0996422215
-size 33511
+oid sha256:16849f3419a63f698de5b3c8ff826e7e87a9ee95f807497ddb629ee5cb9461b1
+size 33636
