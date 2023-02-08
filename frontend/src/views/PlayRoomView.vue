28f0c9ae5537b1087a586574ec68af6f47df13ba
OctoHun <tjd_gns12@naver.com>
2023-02-08 17:53:19 +0900

    FE-FIX : 파비콘, 타이틀, 같이하기 디테일 수정(#41)
    Former-commit-id: ab532a52f3beea9659846a2abc62d7e28c1e6596

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43cb4097b39a18515fb0169755a762eea7f4789b4a2567a03180ce65433d87ac
-size 33881
+oid sha256:6cb13e5170f16a6b69c5be1ec65dc3d76cb8f33c708191daf835908107ed5de8
+size 34038
