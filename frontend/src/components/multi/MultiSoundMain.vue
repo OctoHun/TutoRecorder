b3f44670aa0c1533265d428ab96d28ac3acf7845
OctoHun <tjd_gns12@naver.com>
2023-02-08 14:19:15 +0900

    FE-FIX : 같이하기-대기방 마우스포인터 수정(#41)
    Former-commit-id: cbbaeb2c1e0ec8338ed00e74a8e7ed990f6d4214

--- frontend/src/components/multi/MultiSoundMain.vue
+++ frontend/src/components/multi/MultiSoundMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:594ee1d1773b122264c6dbd835affa7c0b4dde3b3e9fd55682e18fe930f3d74c
-size 20451
+oid sha256:2c1d080c31457ac5e5b087cd2d925f0a72169e47cd7acc1cb8efd597e2385bf0
+size 20531
--- frontend/src/components/solo/SoloSoundMain.vue
+++ frontend/src/components/solo/SoloSoundMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d64671443be5ece0865616d8714773fee91cbd2bae5df48dd1c3d43c4f9f5f56
-size 17723
+oid sha256:9a55f1b8c2f9775ded22f8ab7a8aaf3ed14aca6cb6805732b668d077e2e04555
+size 18338
