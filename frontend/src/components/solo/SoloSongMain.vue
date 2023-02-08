b0af27ba901264e1251e0263620ed10b1401c1af
이우승 <ss6116@daum.net>
2023-02-08 11:24:45 +0900

    EXTRA : merge전 commit
    Former-commit-id: 862c6adb5016947a488cfa2d801ae2b7572e55c5

--- frontend/src/components/solo/SoloSongMain.vue
+++ frontend/src/components/solo/SoloSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7943a8182659d26cf10d91afb286c442d1245afe95185cefcb8846b1d015777e
-size 33232
+oid sha256:b9247c550fb8aa250246115a9eec82310c7735243ce61200e2c211db09ba7c9e
+size 33209
