9e1c58c6e6751c7bfeb97ad021eb3d2230307261
임태민 <devtaemin@gmail.com>
2023-02-08 14:57:11 +0900

    FE-FEAT : 같이하기 동시접속 구현 (#46)
    Former-commit-id: f865579c815ef5f0d13d7885354f61ac57909d0c

--- frontend/src/components/solo/SoloSoundMain.vue
+++ frontend/src/components/solo/SoloSoundMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d64671443be5ece0865616d8714773fee91cbd2bae5df48dd1c3d43c4f9f5f56
+oid sha256:57d99ff6f60dd131524d2411950482eb065b80115bc22c86e3dbb55543bec400
 size 17723
--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4565b12c6cce38880f37c55216902a8ebbacec9df0e0b104005e7a0996422215
-size 33511
+oid sha256:bfd4413ea477e1b30022f110328fc2544c2edbdbd02d2c085876fa6c9021fc9e
+size 34198
