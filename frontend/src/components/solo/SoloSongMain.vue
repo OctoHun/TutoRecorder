3fb7b543216459d4e541ea4c7482e9896aaa2084
njh3321 <njh3321@naver.com>
2023-02-08 17:48:45 +0900

    FE-FEAT : 곡 목록 select-box 추가 준비 (#39)
    Former-commit-id: 02e88adf85ba575115d2745547a9d9d3fd2a58f5

--- frontend/src/components/solo/SoloSongMain.vue
+++ frontend/src/components/solo/SoloSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fc2bd77d79ecb278ae0a1659098931ab9725fead24b10943448222c749fc99a
-size 37089
+oid sha256:bebf5ffd5a03d520e6b42671c3fdce91e70c4f63a51bb19f68030fcf21258fec
+size 37458
