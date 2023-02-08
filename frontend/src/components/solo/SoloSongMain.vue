79a7ef23e1e30caa3347e325bf28957be1c2c82e
njh3321 <njh3321@naver.com>
2023-02-08 17:41:12 +0900

    FE-FEAT : 혼자하기-연주하기의 check box에 switch 기능 추가(#39)
    Former-commit-id: b4bdfe453eb0f229f4b4fe417ac60c0fc51c157f

--- frontend/src/components/solo/SoloSongMain.vue
+++ frontend/src/components/solo/SoloSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32213a4ca69398d1df03a8143dda9e6004a70b347a907eef43d95849b60fb631
-size 34272
+oid sha256:2fc2bd77d79ecb278ae0a1659098931ab9725fead24b10943448222c749fc99a
+size 37089
