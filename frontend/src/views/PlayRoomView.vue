85e0a7b9829f1c693f2e48b3db8da8f29318ab37
pjt laptop <munhwa3278@gmail.com>
2023-02-09 16:11:02 +0900

    FE-FIX : 비디오 위치 수정 완료 (#52)
    Former-commit-id: c14cb17745be61bfa33a09629e82c995971bf20f

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b2561f416f21963467c3fd7a0919a220ee0dcb6f75dd4af9b5fc92878ab6268
-size 37409
+oid sha256:9b3cdb8d8fed7439d500664762af45462417eb0e889cb80185a5879b0fbcbb97
+size 37369
