5628317dc606e4181bd949841588975a7c68fcd2
OctoHun <tjd_gns12@naver.com>
2023-02-09 15:43:26 +0900

    FE-FEAT : 레디여부 표시(#49)
    Former-commit-id: 733cf6c4af5dd40cf5477df4aa2b5d34a28d71fa

--- frontend/src/components/ModeMain.vue
+++ frontend/src/components/ModeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db945ba795ad81ed1b7a47fe18ec20dcdd7a76321692d106b8871e3319f23be2
-size 7044
+oid sha256:6caad529a2bb0bb70799310cf98e7fefff49fd9f017ea08e55787a846a3fb4a6
+size 7078
