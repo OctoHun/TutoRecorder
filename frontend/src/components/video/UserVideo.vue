9353c9962f45db62c802f648582f6e8206bdcac8
OctoHun <tjd_gns12@naver.com>
2023-02-09 15:43:26 +0900

    FE-FEAT : 레디여부 표시(#49)
    Former-commit-id: bfd3a321f5b985bad93038d83411a211f3afb7e3

--- frontend/src/components/video/UserVideo.vue
+++ frontend/src/components/video/UserVideo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01247a874f339ab35bb0d555c06ba21125799bb1ef297bd178dd61570bcba3e1
-size 1004
+oid sha256:409faa36b3d1562b53df7969821b53ebb7a868440b54ded213238490c59cbc23
+size 975
--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b2561f416f21963467c3fd7a0919a220ee0dcb6f75dd4af9b5fc92878ab6268
-size 37409
+oid sha256:57d7bc0b06f24b212fc35ffec170ce146adf955e6d0e91aad889bad36ae934fa
+size 38860
