ec432c406196ad0b3256a671795e7de6e4a0051c
jongukchoi95 <jongukchoi95@gmail.com>
2023-02-09 15:03:07 +0900

    FE-FEAT : 운지법 구분 기능 추가(#51)
    Former-commit-id: 31003db1b2cbe856694b3f51ebfeed4a39c6e506

---
+++ frontend/src/components/solo/SoloFingeringMain.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a796ce52c8988cb6a1fe0e5c2769e03df30f08ed763cbc5422f6181b0dfe2197
+size 17085
--- frontend/src/components/solo/SoloSoundMain.vue
+++ frontend/src/components/solo/SoloSoundMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d64671443be5ece0865616d8714773fee91cbd2bae5df48dd1c3d43c4f9f5f56
-size 17723
+oid sha256:1f86567c2f23857e3ee29c253956e1d043bf4a0db1f203194fc08cdea5f44a56
+size 22567
