20c288344995fd5ce8d996a7f754e07a2ee0199a
이우승 <ss6116@daum.net>
2023-02-10 17:39:24 +0900

    FE-FEAT : 곡 연습 점수 기능 추가(#22)
    Former-commit-id: 72bc2e6aab5289417fb0a15d7c23c888b5f50fdc

--- frontend/src/components/solo/SoloSongMain.vue
+++ frontend/src/components/solo/SoloSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e73fcbe797ad14783b8a26dd0a80536e64c587a04df7da05b566926fc94bedeb
-size 38172
+oid sha256:0c85765fb49f121bf0d458e091aba091282008bd4cca2372dcfa95143f6739ce
+size 39704
