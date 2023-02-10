f907d1ae0a99af2a6dd6f0f538a52cc567f7d429
pjt laptop <munhwa3278@gmail.com>
2023-02-10 13:33:06 +0900

    FE-FEAT : 혼자하기 운지법 버튼 생성 및 연결(#48)
    Former-commit-id: d2cbf5f67bc8eef810676bc7ddde7cb7ee651e62

--- frontend/src/components/solo/SoloFingeringMain.vue
+++ frontend/src/components/solo/SoloFingeringMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84a5a7d4c3299aeca74865a97a59a533bf633568cdc73f1ce91712b4d26f35b4
-size 17277
+oid sha256:cd63d673e2bc360819774a940daa5a7c5e00e926481134b606e7d658cdd1fe3b
+size 17072
