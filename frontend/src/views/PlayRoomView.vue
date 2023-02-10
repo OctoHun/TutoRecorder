754efcdd5956b96c5b77bf4d4ecf07c1231aa9cc
pjt laptop <munhwa3278@gmail.com>
2023-02-10 15:06:57 +0900

    FEAT-FRONT : 운지법 버튼 추가 및 연결(#48)
    Former-commit-id: ddd162b88f14c4c22defe5223f6a852609f4a377

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7358ef65b74fcc9d77aafb45209de0be723dd43fa5517df962af567f7b98f2e
-size 43350
+oid sha256:831d157a17bccbdc34caae29739285788377374d455c80b6303013f04b7575b3
+size 43351
