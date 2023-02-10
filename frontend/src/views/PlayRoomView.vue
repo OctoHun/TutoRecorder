f653cd9dd0ecfb160a71dbf706e8090221e92b93
pjt laptop <munhwa3278@gmail.com>
2023-02-10 14:38:50 +0900

    FE-FIX : 같이하기 코드 공유 디자인 수정(#58)
    Former-commit-id: de8c7d5dbf8ca5bdd86a430a2d5a2b91c6fedcab

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7358ef65b74fcc9d77aafb45209de0be723dd43fa5517df962af567f7b98f2e
-size 43350
+oid sha256:8b85da509870e7304aee767f4762406a8c7f9233e13e835ae8322d280051acda
+size 43777
