b8a11a66758b90d2d5a972a43fb4500e4cf5867d
OctoHun <tjd_gns12@naver.com>
2023-02-10 14:51:22 +0900

    FE-FIX : 같이하기 게임 후 준비완료가 유지되던 것 수정(#55)
    Former-commit-id: 4c465aa3c7b9edf7de86301c0beb574b6c214b10

--- frontend/src/components/multi/MultiSoundMain.vue
+++ frontend/src/components/multi/MultiSoundMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea099964197a903e65d1b130cce2c5d39b3605ed6b19593f78e7cb220816628d
-size 15317
+oid sha256:e931d3ea3a1bc540a16ab91df65e13f56cba1df335551268e3f9e75376f4fec1
+size 15328
--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7358ef65b74fcc9d77aafb45209de0be723dd43fa5517df962af567f7b98f2e
-size 43350
+oid sha256:fe76ae5f2be192f4ff6a88e632b0ba2a61a20c8a21db5b732ac3b87030d90317
+size 43509
