06243746e48b1822565976fc2f8e7d960dc6773b
노준호 <njh3321@naver.com>
2023-02-12 04:00:37 +0900

    FE-FIX : 같이하기 대기방 UI 수정 및 혼자하기- sound 양 옆 간격 동일하게 수정(#59)
    Former-commit-id: 1ff2eae38736cd539e5a18004be161a9e14a2560

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a329c5b340ff711456d5423943483f753d1e118fa4cda0d081a0afcbe58a57a5
-size 44064
+oid sha256:686c8708431d67c117e6a38c75b6e7e67c51da211423cb50ebcf66edfa26e9f4
+size 44461
