c89a22c101592941f50c74aae0659e5e0175111a
노준호 <njh3321@naver.com>
2023-02-12 19:58:35 +0900

    FE-FIX : 첫 화면 애니메이션을 app.vue에서 HomeMainvue로 이동 (#22)
    Former-commit-id: c0600df68e2b08c7b107d86922ced50fcb4459fe

--- frontend/src/components/HomeMain.vue
+++ frontend/src/components/HomeMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b94614c9f2910af2fd9b069151d49f72988f81fab9d8f5501adaa2f7e0ad5612
-size 3013
+oid sha256:c4e6c4dd5404a19a1e8e2780c7c0d9dcd58ef248da9cb7b29f6dab6b55d9a8f0
+size 6424
