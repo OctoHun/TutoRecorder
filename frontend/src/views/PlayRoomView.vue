7fec4a22c5188b9641f952d69ee2dad654e99ecf
노준호 <njh3321@naver.com>
2023-02-12 11:26:47 +0900

    FE-FIX : 같이하기 대기방 게임설정 패널 및 게임 설정 div 디자인 수정
    Former-commit-id: be0b557196c3fc7c1135dd2021f9a925f86dfed4

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:686c8708431d67c117e6a38c75b6e7e67c51da211423cb50ebcf66edfa26e9f4
-size 44461
+oid sha256:b07bfe2506ea9a1aaf13fdb87994dad03ccfbc6eb85b213e4b10a7159277fd2e
+size 44556
