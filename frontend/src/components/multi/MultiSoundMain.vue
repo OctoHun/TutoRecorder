6f2573ce69e5c09538e345e6e36ea851d649b41e
OctoHun <tjd_gns12@naver.com>
2023-02-12 16:05:56 +0900

    FE-FEAT : 대기방, 분석 기능 추가(#55)
    Former-commit-id: 187981d0df4ca2db86d2021920270faba1020d0f

--- frontend/src/components/multi/MultiSoundMain.vue
+++ frontend/src/components/multi/MultiSoundMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e931d3ea3a1bc540a16ab91df65e13f56cba1df335551268e3f9e75376f4fec1
-size 15328
+oid sha256:deeac893b7f244c7d38ed50cd4b20ca66247828114b2778f83aa398a468ab077
+size 15329
