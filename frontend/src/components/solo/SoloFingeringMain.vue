34c2b8a628f2b2cf93405bf01319feb64f66b587
OctoHun <tjd_gns12@naver.com>
2023-02-12 17:18:56 +0900

    FE-FEAT : 게임 기능 수정, 혼자하기-분석 완료(#55)
    Former-commit-id: 1aefe1ae56556ed0522f07940b6486492071c853

--- frontend/src/components/solo/SoloFingeringMain.vue
+++ frontend/src/components/solo/SoloFingeringMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee5646e1190b394c0103ce4bf4cf008e2ce75be42f87c5033dd17952435d0a1d
-size 17566
+oid sha256:e26fda7b7e7e563e88a59b5f6de8e738fffaf5cf4186a3c09d04934237cb0297
+size 17569
--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20a4eb8b8d3651b0510244655b2e93eba296caf212701d88fe8060df74adc4cb
-size 47223
+oid sha256:c480c133bf720b00e0de521c712894cf6f9a03633a8fc7bc8b78eeb4d08a9bab
+size 47259
