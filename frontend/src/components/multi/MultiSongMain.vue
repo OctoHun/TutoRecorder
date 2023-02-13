2bf336514dc28be2ae30bd7ee65a7cdb9108ed18
OctoHun <tjd_gns12@naver.com>
2023-02-13 16:26:24 +0900

    FE-FIX : 멀티-사운드 나갈 경우 게임 초기화(#64)
    Former-commit-id: fdee432eb0c7e6c1a020ad5a4f293b4599aa5bff

--- frontend/src/components/multi/MultiSongMain.vue
+++ frontend/src/components/multi/MultiSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbc134d337bbc8b2e1dc295effb7fdffdfd780c053a6c4b9202a0ac5856c1fa6
-size 40504
+oid sha256:8f5230a98e834be97b736e8faae975f465d8d639cba2a6a8e296afe963324d95
+size 40570
