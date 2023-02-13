7d9144646bd4cb765b36c9538f99d46ada281362
임태민 <devtaemin@gmail.com>
2023-02-13 17:10:16 +0900

    FE-FIX : 같이하기 연주하기 방장 제어 동시 게임 실행 기능 추가 (#64)
    Former-commit-id: fbafe9e8d1852ce998ac66aa9d71dda42208ed54

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ba84abb0678914e49c5f4db2a7c00dbbf1edf29267b9cb50ea0d5e83ff82800
-size 48515
+oid sha256:45e47f845e9cfd57dd39a3996fcd13f8023a7b2df9c3d82b44f268614d9c16ec
+size 50078
