86fb78830f7bff5cace9c2397f3ce5043493eb29
임태민 <devtaemin@gmail.com>
2023-02-13 10:59:59 +0900

    FE-FIX : 같이하기 대기방 게임정보 디스플레이 수정 (#64)
    Former-commit-id: eebc5f0f271e5e9191035c279b7654e82fb5cfad

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c45cccdbc8a062e9d82c5306e52b7e133768e3552d09c2641e32f63a1c8ef641
-size 47743
+oid sha256:d418026583cb8cb3a41c3efe0fb4b52328b8f1106928b602ff82b7345b6adcef
+size 47723
