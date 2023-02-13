5a551ebd8aacd1a633614e718d59e9b718f253be
OctoHun <tjd_gns12@naver.com>
2023-02-13 16:26:24 +0900

    FE-FIX : 멀티-사운드 나갈 경우 게임 초기화(#64)
    Former-commit-id: a3ecf5a418e325403a58cac84d31020ff8ad9f0f

--- frontend/src/views/PlayRoomView.vue
+++ frontend/src/views/PlayRoomView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d418026583cb8cb3a41c3efe0fb4b52328b8f1106928b602ff82b7345b6adcef
-size 47723
+oid sha256:aee6861acb032afcc5b79bb47af092ed29c642dae35756b5f746512f8029e901
+size 47370
