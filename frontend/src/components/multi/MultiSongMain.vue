63e6270d7cfe67aec9ec4eb4a62553f50b68539c
임태민 <devtaemin@gmail.com>
2023-02-13 14:25:31 +0900

    FE-FIX : 같이하기 연주하기 곡 선택 보여주기 수정 (#64)
    Former-commit-id: 0e5b845b40f21ee8da0ca6efcfe4d4ac54e56f9f

--- frontend/src/components/multi/MultiSongMain.vue
+++ frontend/src/components/multi/MultiSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8cd9c8d788b66e55ca5171873b61a61932b8aa1c9c8ad6268eda8f5043a1cbc
-size 40307
+oid sha256:bbc134d337bbc8b2e1dc295effb7fdffdfd780c053a6c4b9202a0ac5856c1fa6
+size 40504
