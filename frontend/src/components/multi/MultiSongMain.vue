eb809d7089e6d6474ba55c9d35527065ceb686ec
이우승 <ss6116@daum.net>
2023-02-14 10:30:19 +0900

    FE-REF : 메인 Merge전에 리펙토링(#65)
    Former-commit-id: 8348a7ac9a6c55ba703fe20a43977249a340a32a

--- frontend/src/components/multi/MultiSongMain.vue
+++ frontend/src/components/multi/MultiSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4db602726c89f8e91e24c89640e739b42682145fa48dc44896f2db921b4f5c98
-size 42320
+oid sha256:4bd3af0cb32be2d2fbe7ed95fbc38940d485621e46a7abf1f105053241b3b0ba
+size 40797
--- frontend/src/components/solo/SoloSongMain.vue
+++ frontend/src/components/solo/SoloSongMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e23b18f36a8a83ee568b7143edacfc50083bbe7d472f7a3622738128c9da0ed0
-size 42117
+oid sha256:137c6339e3bd7103b28ea4b5204b00d09925bfe50b3ee7457a2c22ab20e84433
+size 41243
