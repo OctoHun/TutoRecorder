7a4654d1482a72de6c128fbb20aedc4cbbf3cb56
njh3321 <njh3321@naver.com>
2023-02-15 12:02:25 +0900

    REFACTOR : App.vue 리팩토링
    Former-commit-id: 657031c924d1294873fbd3ab2de90dfec7906a35

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,3 @@
-<template>
-  <div>
-    <div id="background-image">
-      <router-view/>
-    </div>
-  </div>
-</template>
-
-<script>
-
-export default {
-    name: 'App',
-    components: {
-
-    },
-    data() {
-      return {
-        
-      }
-    },
-    methods: {
-      
-      
-    },
-    
-}
-</script>
-<style>
-  
-</style>
+version https://git-lfs.github.com/spec/v1
+oid sha256:f33247607fc14e1ddc0e4d3cceee0e1c47edd5612be96e3a37024ccadc7b735b
+size 267
